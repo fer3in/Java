91bf93b60312a3034a65012d1d3516cbe625d182
alice/alex <121223171+fer3in@users.noreply.github.com>
2022-12-22 12:03:45 +0300

    Create Shape.java

---
+++ task3/Shape.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+
+public abstract class Shape
+{
+    protected String color;
+
+    protected boolean filled;
+
+    public String getColor()
+    {
+        return this.color;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+    public boolean isFilled()
+    {
+        return this.filled;
+    }
+    public void setFilled(boolean filled)
+    {
+        this.filled = filled;
+    }
+    public abstract double getArea();
+    public abstract double getPerimeter();
+    public abstract String toString();
+}
